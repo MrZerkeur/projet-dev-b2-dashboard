0f8fbbde687caedd1bc3c4a223c836c7c75377e2
axel <axel.broquaire@outlook.fr>
2024-03-15 09:24:20 +0100

    added DB to docker compose - dev version

---
+++ init.sql
@@ -0,0 +1,34 @@
+use db_projet_dev;
+
+CREATE table if not exists db_projet_dev.users (
+	user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	first_name varchar(30) not null,
+	name varchar(50) not null,
+	mail varchar(100) unique not null,
+	password_hash varchar(62) not null,
+	salt varchar(15) not null
+);
+
+CREATE table if not exists db_projet_dev.sites (
+	site_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	name varchar(30),
+	user_id integer,
+	foreign key (user_id) references users(user_id)
+);
+
+CREATE table if not exists db_projet_dev.pictures (
+	picture_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	name varchar(30)not null,
+	image_path text not null,
+	section_name varchar(50) not null,
+	site_id integer,
+	foreign key (site_id) references sites(site_id)
+);
+
+CREATE table if not exists db_projet_dev.texts (
+	text_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	content text not null,
+	section_name varchar(50) not null,
+	site_id integer,
+	foreign key (site_id) references sites(site_id)
+);

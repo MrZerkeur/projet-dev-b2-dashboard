8d425c4b304ea7cdd74ac997ca03b622bd47cf96
axel <axel.broquaire@outlook.fr>
2024-04-09 10:16:01 +0200

    can now add images

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, send_file
 import mysql.connector
 import requests
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -13,23 +14,9 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor()
 
-
-# @app.route("/sites/<id_site>/images", methods=['GET'])
-# def get_images(id_site):
-#     url = 'http://file-server:8000/chat.jpeg'
-#     filename = 'chat.jpeg'
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         with open(filename, 'wb') as f:
-#             f.write(response.content)
-#         return send_file(filename, mimetype='image/jpeg')
-#     else:
-#         return "Failed to download image."
-
-
 @app.route("/sites/<id_site>/images", methods=['GET'])
 def get_all_images(id_site):
-    cursor.execute('SELECT name, image_path, section_name, site_id from db_projet_dev.images p where site_id = %s;', (id_site,))
+    cursor.execute('SELECT name, image_path, section_name, site_id from db_projet_dev.images p where site_id = "%s";', (id_site,))
     rows = cursor.fetchall()
     
     images_data = []
@@ -45,16 +32,69 @@ def get_all_images(id_site):
             'name': row[0],
             'image_base64': image_data_base64,
             'section_name': row[2],
-            'site_id': row[3]
+            'site_id': row[3],
         }
         images_data.append(image_data)
     
     return jsonify({'images_data': images_data})
 
+#Normalement ça marche mais à tester, attendre que les modifications soient faites sur la DB
+@app.route("/sites/<id_site>/images/<image_id>", methods=['GET'])
+def get_specific_image(id_site, image_id):
+    cursor.execute('SELECT name, image_path, section_name from db_projet_dev.images p where image_uuid = "%s" and site_uuid = "%s";', (id_site, image_id,))
+    rows = cursor.fetchall()
+
+    images_data = []
+    for row in rows:
+        image_path = row[1]
+        image_url = f"http://file-server:8000/{image_path}"
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            image_data_base64 = base64.b64encode(response.content).decode('utf-8')
+        else:
+            image_data_base64 = None
+            image_data = {
+                'name': row[0],
+                'image_base64': image_data_base64,
+                'section_name': row[2],
+            }
+        images_data.append(image_data)
+    
+    return jsonify({'images_data': images_data})
+
+# Reçoit le base64 et le path dans un json
+@app.route("/sites/<id_site>/images", methods=["POST"])
+def add_image(id_site):
+    if not request.json:
+        return jsonify({'error': 'No JSON data received'}), 400
+
+    if 'base64_encoded_image' not in request.json or 'path' not in request.json:
+        return jsonify({'error': 'Missing required fields in JSON data'}), 400
+
+    base64_encoded_image = request.json['base64_encoded_image']
+    path = request.json['path']
+
+    try:
+        directory = os.path.dirname(path)
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        binary_data = base64.b64decode(base64_encoded_image)
+
+        with open(path, 'wb') as file:
+            file.write(binary_data)
+
+        return jsonify({'message': "Image added successfully",
+                        'path': path}), 201
+    except Exception as e:
+        return jsonify({'message': "An error occurred",
+                        'error': str(e)}), 500
+
 
 @app.route("/sites/<id_site>/texts", methods=['GET'])
 def get_texts(id_site):
-    cursor.execute('SELECT content, section_name from db_projet_dev.texts t WHERE site_id = %s;', (id_site,))
+    cursor.execute('SELECT content, section_name from db_projet_dev.texts t WHERE site_id = "%s";', (id_site,))
     texts = cursor.fetchall()
     texts_json = [{'content': text[0], 'section_name': text[1]} for text in texts]
 
@@ -62,44 +102,3 @@ def get_texts(id_site):
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
-# @app.route("/sites/<id_site>/images", methods=['GET'])
-# def get_images(id_site):
-#     cursor.execute('SELECT name, section_name from db_projet_dev.images p WHERE site_id = %s;', (id_site,))
-#     results = cursor.fetchall()
-#     # for result in results:
-#     #     name, section_name = result
-#     #     to_return = name, section_name, id_site, result, results
-#     #     return to_return
-#     print(name)
-#     print(section_name)
-#     print(id_site)
-#     print(result)
-#     print(results)
-#     url = f'http://file-server:8000/{id_site}/{section_name}/{name}'
-#     filename = f'{name}'
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         with open(filename, 'wb') as f:
-#             f.write(response.content)
-#     else:
-#         return "Failed to download image."
-    
-    # If all images are downloaded successfully, send the last image
-    # if results:
-    #     return send_file(filename, mimetype='image/jpeg')
-    # else:
-    #     return "No images found."
-
-    # url = 'http://file-server:8000/chat.jpeg'
-    # filename = 'chat.jpeg'
-    # response = requests.get(url)
-    # if response.status_code == 200:
-    #     with open(filename, 'wb') as f:
-    #         f.write(response.content)
-    #     return send_file(filename, mimetype='image/jpeg')
-    # else:
-    #     return "Failed to download image."
-
-# SELECT name, section_name from db_projet_dev.images p WHERE site_id = 2;
-
--- init.sql
+++ init.sql
@@ -1,40 +1,47 @@
 CREATE DATABASE IF NOT EXISTS db_projet_dev;
 
-use db_projet_dev;
+USE db_projet_dev;
 
-CREATE table if not exists db_projet_dev.users (
-    user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name varchar(30) not null,
-    last_name varchar(50) not null,
-    email varchar(100) unique not null,
-    password_hash varchar(62) not null,
-    salt varchar(15) not null
+CREATE TABLE IF NOT EXISTS db_projet_dev.users (
+    user_id UUID DEFAULT UUID() PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(62) NOT NULL,
+    salt VARCHAR(15) NOT NULL
 );
 
-CREATE table if not exists db_projet_dev.sites (
-    site_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name varchar(30),
-    host varchar(15),
-    port varchar(5),
-    user_id integer not null,
-    foreign key (user_id) references users(user_id)
+CREATE TABLE IF NOT EXISTS db_projet_dev.sites (
+    site_id UUID DEFAULT UUID() PRIMARY KEY,
+    name VARCHAR(30),
+    host VARCHAR(15),
+    port VARCHAR(5),
+    user_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE table if not exists db_projet_dev.images (
-    image_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name varchar(50)not null,
-    image_path text not null,
-    section_name varchar(50) not null,
-    site_id integer not null,
-    foreign key (site_id) references sites(site_id)
+CREATE TABLE IF NOT EXISTS db_projet_dev.sites_users (
+    site_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    FOREIGN KEY (site_id) REFERENCES sites(site_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE table if not exists db_projet_dev.texts (
-    text_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    content text not null,
-    section_name varchar(50) not null,
-    site_id integer not null,
-    foreign key (site_id) references sites(site_id)
+CREATE TABLE IF NOT EXISTS db_projet_dev.images (
+    image_id UUID DEFAULT UUID() PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    image_path TEXT NOT NULL,
+    section_name VARCHAR(50) NOT NULL,
+    site_id UUID NOT NULL,
+    FOREIGN KEY (site_id) REFERENCES sites(site_id)
+);
+
+CREATE TABLE IF NOT EXISTS db_projet_dev.texts (
+    text_id UUID DEFAULT UUID() PRIMARY KEY,
+    content TEXT NOT NULL,
+    section_name VARCHAR(50) NOT NULL,
+    site_id UUID NOT NULL,
+    FOREIGN KEY (site_id) REFERENCES sites(site_id)
 );
 
 INSERT INTO db_projet_dev.users 
@@ -47,16 +54,49 @@ INSERT INTO db_projet_dev.users
 (first_name, last_name, email, password_hash, salt) 
 values ("test2", "test2", "test2@gmail.com", "test", "aze");
 
-INSERT INTO db_projet_dev.sites (name, host, port, user_id) values ('site_1', '1.1.1.1', '10', 1);
-INSERT INTO db_projet_dev.sites (name, host, port, user_id) values ('site_2', '2.2.2.2', '20', 1);
-INSERT INTO db_projet_dev.sites (name, host, port, user_id) values ('site_3', '3.3.3.3', '30', 2);
-INSERT INTO db_projet_dev.sites (name, host, port, user_id) values ('site_3', '3.3.3.3', '30', 3);
 
-INSERT INTO db_projet_dev.texts (content, section_name, site_id) values ('test', 'test', 1);
-INSERT INTO db_projet_dev.texts (content, section_name, site_id) values ('test2', 'test2', 2);
-INSERT INTO db_projet_dev.texts (content, section_name, site_id) values ('test3', 'test2', 2);
+INSERT INTO db_projet_dev.sites (name, host, port, user_id)
+SELECT 'site_1', '1.1.1.1', '10', user_id
+FROM db_projet_dev.users 
+WHERE email = 'test@gmail.com';
 
-INSERT INTO db_projet_dev.images (name, section_name, site_id, image_path) VALUES ('test1.jpg', 'test1', 1, CONCAT(site_id, '/', section_name, '/', name));
-INSERT INTO db_projet_dev.images (name, section_name, site_id, image_path) VALUES ('test2.jpg', 'test2', 2, CONCAT(site_id, '/', section_name, '/', name));
-INSERT INTO db_projet_dev.images (name, section_name, site_id, image_path) VALUES ('test3.jpg', 'test3', 2, CONCAT(site_id, '/', section_name, '/', name));
-INSERT INTO db_projet_dev.images (name, section_name, site_id, image_path) VALUES ('test4.jpg', 'test2', 2, CONCAT(site_id, '/', section_name, '/', name));
+INSERT INTO db_projet_dev.sites (name, host, port, user_id)
+SELECT 'site_2', '2.2.2.2', '20', user_id
+FROM db_projet_dev.users 
+WHERE email = 'test1@gmail.com';
+
+INSERT INTO db_projet_dev.sites (name, host, port, user_id)
+SELECT 'site_3', '3.3.3.3', '30', user_id
+FROM db_projet_dev.users 
+WHERE email = 'test1@gmail.com';
+
+INSERT INTO db_projet_dev.sites (name, host, port, user_id)
+SELECT 'site_4', '4.4.4.4', '40', user_id
+FROM db_projet_dev.users 
+WHERE email = 'test2@gmail.com';
+
+
+INSERT INTO db_projet_dev.texts (content, section_name, site_id)
+SELECT 'test2', 'test2', sites.site_id
+FROM db_projet_dev.sites 
+INNER JOIN db_projet_dev.users ON sites.user_id = users.user_id
+WHERE users.email = 'test1@gmail.com';
+
+INSERT INTO db_projet_dev.texts (content, section_name, site_id)
+SELECT 'test2', 'test2', sites.site_id
+FROM db_projet_dev.sites 
+INNER JOIN db_projet_dev.users ON sites.user_id = users.user_id
+WHERE users.email = 'test1@gmail.com';
+
+
+INSERT INTO db_projet_dev.images (name, section_name, site_id, image_path) 
+SELECT 'test1.jpg', 'test1', site_id, CONCAT(site_id, '/', 'test1', '/', 'test1.jpg')
+FROM db_projet_dev.sites
+INNER JOIN db_projet_dev.users ON sites.user_id = users.user_id
+WHERE users.email = 'test@gmail.com';
+
+INSERT INTO db_projet_dev.images (name, section_name, site_id, image_path) 
+SELECT 'test2.jpg', 'test2', site_id, CONCAT(site_id, '/', 'test2', '/', 'test2.jpg')
+FROM db_projet_dev.sites
+INNER JOIN db_projet_dev.users ON sites.user_id = users.user_id
+WHERE users.email = 'test@gmail.com';
